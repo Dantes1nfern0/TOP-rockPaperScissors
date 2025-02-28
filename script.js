7b8fda9960255cbe230ee71460afc887a28701c4
DillySong <117123728+DillySong@users.noreply.github.com>
2025-02-28 14:01:21 -0500

    Add base functions for greater then game
    - Added function that plays five rounds.
    - Added messages that show which rounds are being played and the current
    score.
    - Added a final score message and a function that keeps track.

--- script.js
+++ script.js
@@ -1,18 +1,56 @@
+// keeping score
+let humanScore = 0;
+let computerScore = 0;
+let roundNumber = 1;
+
+// then computer input
 function getComputerChoice() {
    let number = Math.floor(Math.random() * 100) + 1;
    return number;
 }
+
+//round consists of getting user input
 function getHumanChoice() {
     let number = prompt('pick a number');
     return parseInt(number);
 }
 
-function determine(a, b) {
-    if (a > b) {
-        return console.log(a + 'wins');
-    } else if (a < b) {
-        return console.log(b + 'wins');
+// then matching the two together
+function determine(computer, human) {
+    if (computer > human) {
+        return (computerScore++, 'Computer: ' + computer + ' | Human: ' + human + ' Computer Wins');
+    } else if (computer < human) {
+        return (humanScore++, 'Computer: ' + computer + ' | Human: ' + human + ' Human Wins');
     } else {
-        return console.log('it\'s a tie');
+        return;
     }
 }
+
+// outputting the result
+function playRound(computer, human) {
+    return( 
+        console.log(('Round ' + roundNumber)),
+        console.log(determine(computer, human)),
+        roundNumber++
+    )
+}
+
+function playFiveRounds() {
+    console.log(playRound(getComputerChoice(), getHumanChoice()));
+    console.log(playRound(getComputerChoice(), getHumanChoice()));
+    console.log(playRound(getComputerChoice(), getHumanChoice()));
+    console.log(playRound(getComputerChoice(), getHumanChoice()));
+    console.log(playRound(getComputerChoice(), getHumanChoice()));
+    let winMessage = (humanScore > computerScore) ? ('Human Wins!') : ('Computer Wins!');
+    
+    return (
+        console.log('Final score: Human(' + humanScore + ') Computer(' + computerScore + ') ' + winMessage)
+    )
+}
+
+playFiveRounds();
+
+
+
+//play a round
+// replaying each round 5 times
