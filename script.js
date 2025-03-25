fef5251a9e0c10792832aa1ec42f0ae32f195f9f
DillySong <117123728+DillySong@users.noreply.github.com>
2025-03-25 10:12:46 -0400

    Add RPS base functions to console

--- script.js
+++ script.js
@@ -1,98 +1,78 @@
-// Keep score
+// keeping score
 let roundNumber = 1;
 let humanScore = 0;
 let computerScore = 0;
 
-// then matching the two together
-function determineWhoWins(computer, human) {
-    const roundCount = document.querySelector('.round-count');
-    roundCount.textContent = ('Round ' + roundNumber)
-    
-    const humanScoreDisplay = document.querySelector('.human-score');
-    const computerScoreDisplay = document.querySelector('.computer-score');
-    function roundScore() {
-        if (computer > human) {
-            return computerScore++;
-        } else if (computer < human) {
-            return humanScore++;
-        }
-    }
-    roundScore()
-    humanScoreDisplay.textContent = (`Human Score: ${humanScore}`);
-    computerScoreDisplay.textContent = (`Computer Score: ${computerScore}`);
-    
-    
-    const roundMessage = document.querySelector('.round-message');
-    function roundMessageFunc() {
-        if (computer > human) {
-            return ('-- Computer Wins -- | Computer: ' + computerChoice + ' | Human: ' + humanChoice);
-        } else if (computer < human) {
-            return (' -- Human Wins -- | Computer: ' + computerChoice + ' | Human: ' + humanChoice);
-        } else {
-            return ('-- No Winner -- | It\'s a tie!');
-        }
-    }
-
-    roundMessage.textContent = roundMessageFunc()
+function getComputerChoice() {
+   let number = Math.floor(Math.random() * 100) + 1;
+   function convertToRPS(number) {
+       if (number <= 33) {
+           return 'paper';
+       } else if (number <= 66) {
+           return 'rock';
+       } else {
+           return 'scissors';
+       }
+   } 
+   return convertToRPS(number);
 }
 
-function logicRockPaperScissors(computer, human) {
-    computerChoice = computer;
-    humanChoice = human;
-    if (computer === human) {
-        return determineWhoWins(0, 0);
-    } else if (
-        computer === 'rock' && human === 'scissors' ||
-        computer === 'paper' && human === 'rock' ||
-        computer === 'scissors' && human === 'paper'
-    ) {
-        return determineWhoWins(1, 0);
-    } else {
-        return determineWhoWins(0, 1);
+const playerButtons = document.querySelectorAll('.player');
+playerButtons.forEach((element) => element.addEventListener('click', getHumanChoice));
+
+function getHumanChoice(e) {
+    rpsArray = ['rock', 'paper', 'scissors'];
+    let humanChoice;
+    for (i = 0; i < rpsArray.length; i++) {
+        if (e.target.classList.contains(rpsArray[i]) == true) {
+            humanChoice = rpsArray[i];
+        }
     }
+    playRound(getComputerChoice(), humanChoice);
 }
 
 function playRound(computer, human) {
-    const resetContainer = document.querySelector('#reset-container');
-    const resetButton = document.createElement('button');
-    resetButton.textContent = ('Play Again?');
-    resetButton.addEventListener('click', () => {
-        window.location.reload();
-    })
-    
-    logicRockPaperScissors(computer, human);
-    roundNumber++;
-    const result = document.querySelector('.results');
-    
-    if (humanScore === 5) {
-        result.textContent = 'Human Wins!';
-        buttonsContainer.forEach((button) => button.removeEventListener('click', getHumanChoice));
-        resetContainer.appendChild(resetButton);   
-    } else if (computerScore === 5) {
-        result.textContent = 'Computer Wins!';
-        buttonsContainer.forEach((button) => button.removeEventListener('click', getHumanChoice));
-        resetContainer.appendChild(resetButton);
+    console.log(`Round: ${roundNumber++}`)
+    console.log(logicRockPaperScissors(computer, human));
+
+    // Game end check
+    if (computerScore != 5 && humanScore != 5) {
+        return
+    } else {
+        playerButtons.forEach((element) => element.removeEventListener('click', getHumanChoice));
+        if (humanScore > computerScore) {
+            console.log('human wins!')
+        } else {
+            console.log('computer wins!')
+        }
     }
 }
 
-function getRockPaperScissors(number) {
-    if (number > 66) {
-        return 'scissors';
-    } else if (number <= 66 && number > 33) {
-        return 'rock';
-    } else if (number <= 33 && number > 0) {
-        return 'paper';
+function logicRockPaperScissors(computer, human) {
+    if (computer == human) {
+        return determine(0, 0);
+    } else if (
+        computer == 'rock' && human == 'scissors' ||
+        computer == 'paper' && human == 'rock' ||
+        computer == 'scissors' && human == 'paper'
+    ) {
+        return determine(1, 0);
+    } else {
+        return determine(0, 1);
+    }
+    function determine(computerOutput, humanOutput) {
+        let winMessage;
+        if (computerOutput > humanOutput) {
+            computerScore++
+            winMessage = '-- Computer won --'
+            console.log('computer score: ' + computerScore)
+        } else if (computerOutput < humanOutput) {
+            humanScore++
+            winMessage = '-- Human won --'
+            console.log('human score: ' + humanScore)
+        } else {
+            winMessage = '-- It\'s a tie --'
+        }
+        return(`Computer: ${computer} | Human: ${human} ${winMessage}`)
     }
 }
-
-function getComputerChoice() {
-    let number = Math.floor(Math.random() * 100) + 1;
-    return getRockPaperScissors(number);
-}
-
-function getHumanChoice(event) {
-    playRound(getComputerChoice(), event.target.id);
-}
-
-const buttonsContainer = document.querySelectorAll('.player')
-buttonsContainer.forEach((button) => button.addEventListener('click', getHumanChoice));
